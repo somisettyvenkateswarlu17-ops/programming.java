1a31c14546c89adb3255204395e22a61e618c68c
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2026-08-22 20:34:31 +0530

    Add program to compare two integers

---
+++ Greater program.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int G = sc.nextInt();
+
+        if (N >= G) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
