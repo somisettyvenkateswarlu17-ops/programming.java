5aea16b8dc54d22f1b07ff7d8942e0941c955602
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2026-08-28 22:07:34 +0530

    Create Digital.java

---
+++ Digital.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int reverse = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+
+        System.out.println(reverse);
+    }
+}
