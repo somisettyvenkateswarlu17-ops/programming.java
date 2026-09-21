d6e1145eef291d7264101dbd55b16ec33be915a7
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2026-09-21 19:08:47 +0530

    Implement sum calculation in Sol class

---
+++ sum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Sol{
+public static void main(String[] arr){
+  Scanner scan = new Scanner(System.in);
+  int a = scan.nextInt();
+  int b = scan.nextInt();
+int   sum = a+b;
+  System.out.print(sum);
+  scan.close();}
+}
