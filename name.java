05f4c7f68448dee02a03dea030d75a09ce38b2ef
SOMISETTY VENKATESWARLU <somisettyvenkateswarlu17@gmail.com>
2026-09-24 21:17:50 +0530

    Add Scanner import and modify main method
    Added import statement and modified main method to read an integer input.

---
+++ name.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sol{
+public static void main(String[] arr){
+  Scanner sc = new Scanner(System.in);
+   int n = sc.nextInt();
+    n = 10;
+  System.out.print(n);
+   }
+                        
+                        
+                        
+    }
